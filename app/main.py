f9b618391278810b854f8f7a036da0e9bf14ab8a
gamasenninn <gamasenninn@yahoo.co.jp>
2021-12-27 11:17:53 +0900

    main修正

--- app/main.py
+++ app/main.py
@@ -32,6 +32,15 @@ def test_view_circular4():
 def test_view_circular5():
     return app.send_static_file('test_view_circular5.html')
 
+@app.route('/test-view-circular6')
+def test_view_circular6():
+    return app.send_static_file('test_view_circular6.html')
+
+@app.route('/test-view-circular7')
+def test_view_circular7():
+    return app.send_static_file('test_view_circular7.html')
+
+
 if __name__ == '__main__':
 
     app.run(host='0.0.0.0',port=5010)
